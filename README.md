0499703e1009aedf8978e305bf888c15287e3f81
gaurav <charpegaurav9@gmail.com>
2018-12-12 13:06:22 +0530

    updated Readmeand comments

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Virtual-Drawing-Pad
 
-A simple application of computer vision using OpenCV library in ubuntu 14.04 LTS.
+A simple application of computer vision using OpenCV library in ubuntu 14.04 LTS. Also works in Ubuntu 16.04.
 
-The progarm uses thresholding function to detect marker in its environment.Then, centroid is 
+Installation Required:
+OS: Ubuntu 14.04 or 16.04 (Tested for these two)
+OpenCV Library: https://docs.opencv.org/3.4.1/d2/de6/tutorial_py_setup_in_ubuntu.html
+
+The program uses thresholding function to detect marker in its environment.Then centroid is
 determined using moments of image. The value corresponding to pixel is changed giving desired color.
 
 The program is able to do following:
-1.Size of detected marker as seen in pc can be increased while drawing giving strokes of various width.
+1.Size of detected marker can be increased while drawing, giving strokes of various width.
 2.Able to choose any color for drawing.
 3.Able to use eraser of variable size.
 4.Marker goes undetected if it is placed away from certain range from camera.
--- vdp.cpp
+++ vdp.cpp
@@ -7,34 +7,38 @@ using namespace std;
 
 int main( int argc, char** argv )
 {
-  VideoCapture cap(0); //capture the video from laptop web cam
+  //capture the video from camera. If laptop webcam is used, keep index = 0 else 1.
+  VideoCapture cap(0);
 
-    if ( !cap.isOpened() )  // if not success, exit program
-    {
+  // if not success, exit program
+  if ( !cap.isOpened() )
+  {
          cout << "Cannot open the web cam" << endl;
          return -1;
-    }
-	 namedWindow("Display", CV_WINDOW_NORMAL); //create a window called "Control"
-	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
-	 namedWindow("color", CV_WINDOW_AUTOSIZE); //create a window called "Color"
+  }
+	namedWindow("Display", CV_WINDOW_NORMAL); //create a window called "Display". It displays color marker drawing
+	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control". Used for  thresholding
+	namedWindow("color", CV_WINDOW_AUTOSIZE); //create a window called "Color". Used for selecting colors
 
-	 int iLowH = 0;
-	 int iHighH = 29;
+  // Set of Values thresholded for particular conditions changes for different ligthing conditions.
+  // H - Hue, S - Saturation, V -Value of a color
+	int iLowH = 0;
+	int iHighH = 29;
 
-	 int iLowS = 96;
-	 int iHighS = 255;
+	int iLowS = 96;
+	int iHighS = 255;
 
-	 int iLowV = 173;
-	 int iHighV = 255;
+	int iLowV = 173;
+	int iHighV = 255;
 
-
-	 int Red=0;
-	 int Green=0;
-	 int Blue=0;
-	 int Black;
-         int size=1;
-	 int wipeoutscreen=0;
-         int save=0;
+  //Initialize colors,size of marker
+	int Red=0;
+	int Green=0;
+	int Blue=0;
+	int Black;
+  int size=1;
+	int wipeoutscreen=0;
+  int save=0;
 
 	//Create trackbars in "Control" window
 	 cvCreateTrackbar("LowH", "Control", &iLowH, 255); //Hue (0 - 179)
@@ -48,113 +52,95 @@ int main( int argc, char** argv )
 
 	////Create trackbars in "color" window
 	 cvCreateTrackbar("Blue", "color", &Blue, 255); //Value (0 - 255)
-	 cvCreateTrackbar("Green", "color", &Green, 255); //Value (0 - 255) 
-	 cvCreateTrackbar("Red", "color", &Red, 255); //Value (0 - 255) 
-	 cvCreateTrackbar("Eraser", "color", &Black,1); //Value (0 - 255) 
-         cvCreateTrackbar("size", "color", &size,30); //Value (0 - 1) 
-         cvCreateTrackbar("wipeoutscreen", "color", &wipeoutscreen,1); //Value (0 - 1) 
-         cvCreateTrackbar("save", "color", &save,1); //Value (0 - 1) 
+	 cvCreateTrackbar("Green", "color", &Green, 255); //Value (0 - 255)
+	 cvCreateTrackbar("Red", "color", &Red, 255); //Value (0 - 255)
+	 cvCreateTrackbar("Eraser", "color", &Black,1); //Value (0 - 255)
+   cvCreateTrackbar("size", "color", &size,30); //Value (0 - 30)
+   cvCreateTrackbar("wipeoutscreen", "color", &wipeoutscreen,1); //Value (0 and 1)
+   cvCreateTrackbar("save", "color", &save,1); //Value (0 and 1)
 
+   //Matrices for storing images
+	 Mat imgOriginal,imgLines,imgHSV,imgThresholded;
 
-
-	 Mat imgOriginal,imgLines;
-       
  	 int n=0;
-    while (true)
-    {
-         n++;
-        
-         bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+   while (true)
+   {
+      n++;
+      // read a new frame from video
+      bool bSuccess = cap.read(imgOriginal);
+
+      if(n==1) ///creates only once imgLines matrix.
+	       imgLines = Mat::zeros(imgOriginal.size(), CV_8UC3 );
+      //if not success, break loop
+      if (!bSuccess){
+        cout << "Cannot read a frame from video stream" << endl;
+        break;
+      }
+
+      //Convert the captured frame from BGR to HSV
+  	  cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
+      //Threshold the image
+  	  inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
+
+  	  //morphological opening (remove small objects from the foreground)
+  	  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+  	  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+  	  //morphological closing (fill small holes in the foreground)
+  	  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+  	  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+  	  //Calculate the moments of the thresholded image
+  	  Moments oMoments = moments(imgThresholded);
+
+  	  double dM01 = oMoments.m01;
+  	  double dM10 = oMoments.m10;
+  	  double dArea = oMoments.m00;
+
+  	  int posX = dM10 / dArea;
+  	  int posY = dM01 / dArea;
+
+  	  int fposX=640-posX;
+
+  	  int xi=fposX,yi=posY;
+  	  int xf=fposX,yf=posY;
+
+      //Varioius Operations
+  	  //Eraser
+      if(Black==1)
+  	   {
+         Red=0;
+         Green=0;
+         Blue=0;
+         size=40;
+       }
+  	  //wipeout screen
+      if(wipeoutscreen==1)
+        imgLines = Mat::zeros(imgOriginal.size(), CV_8UC3 );
+
+      //Checking for distance of obj from camera
+      if(dArea>1700000){
+        cout<<dArea<<endl;
+  	    line(imgLines,Point(xi,yi),Point(xf,yf),Scalar(Blue,Green,Red),size);
+      }
+      //creates circle  based on Centre and Radius.
+      //circle(image,centre,radius,color,thickness,lineType,shift)
+  	  circle( imgOriginal, Point(posX, posY), 2, Scalar( 255, 255, 255 ), 2, 8 );
+
+      //Save image if save is 1
+  	  if(save==1)
+        imwrite("/home/gaurav/Pictures/my_img.jpg",imgLines) ;
 
 
-         if(n==1) ///creates only once imgLines matrix.
-	 imgLines = Mat::zeros(imgOriginal.size(), CV_8UC3 );
+  	  imshow("Display", imgLines); //show the drawn lines
+  	  imshow("Thresholded Image",imgThresholded );//show the thresholded image
+  	  imshow("Original", imgOriginal); //show the original image
 
-         if (!bSuccess) //if not success, break loop
-          {
-             cout << "Cannot read a frame from video stream" << endl;
-             break;
-          }
-	  Mat imgHSV;
-
-	  cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-	 
-	  Mat imgThresholded;
-
-	  inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-	      
-	  //morphological opening (remove small objects from the foreground)
-	  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-	  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-
-	  //morphological closing (fill small holes in the foreground)
-	  dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-	  erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-	  
-	   //Calculate the moments of the thresholded image
-	   Moments oMoments = moments(imgThresholded);
-
-	   double dM01 = oMoments.m01;
-	   double dM10 = oMoments.m10;
-	   double dArea = oMoments.m00;
-	   
-	   int posX = dM10 / dArea;
-	   int posY = dM01 / dArea;
-  
-	   int fposX=640-posX;
-	   
-	   int xi=fposX,yi=posY;
-	   int xf=fposX,yf=posY;
-	  
-	   //Mat imgLines(posX,posY, CV_8UC3, Scalar( 255,255,255));
-
-	   //Eraser	
-           if(Black==1)
-	   {  
-             Red=0;
-             Green=0;
-             Blue=0;
-             size=40;}	
-
-           
-	   //wipeout screen
-            if(wipeoutscreen==1)
-            {
-             imgLines = Mat::zeros(imgOriginal.size(), CV_8UC3 );
-            }
-           //vector<vector<Point> > contours;
-           //vector<Vec4i> hierarchy;
-          // findContours(imgThresholded, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-           
-        if(dArea>1700000) //Checking for distance of obj from camera
-     {
-             
-          
-           cout<<dArea<<endl;
-
-	   line(imgLines,Point(xi,yi),Point(xf,yf),Scalar(Blue,Green,Red),size);
-        }
-          
-
-	   circle( imgOriginal, Point(posX, posY), 2, Scalar( 255, 255, 255 ), 2, 8 );
-           
-	     if(save==1)
-	   {   
-              imwrite("/home/gaurav/Pictures/my_img.jpg",imgLines) ;
-           }     
-
- 
-	   imshow("Display", imgLines); 
-	   imshow("Thresholded Image",imgThresholded );//show the thresholded image
-	   imshow("Original", imgOriginal); //show the original image
-
-           if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-           {
-            cout << "esc key is pressed by user" << endl;
-            break; 
-           }
+      //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+      if (waitKey(30) == 27){
+        cout << "esc key is pressed by user" << endl;
+        break;
+      }
     }
-
-   return 0;
-
+  return 0;
 }
